e8ec2430d5e9e966709e8a663c2d7431066848de
Doug Anderson <doug.anderson@byu.net>
2023-05-08 16:59:11 -0600

    documentations

--- db/src/main/resources/db/migration/V0002__locations.sql
+++ db/src/main/resources/db/migration/V0002__locations.sql
@@ -3,6 +3,6 @@ create table local.locations (
   id char(36) not null PRIMARY KEY,
   location_name varchar(40) not null,
   location_abbreviation varchar(4) not null,
-  country_id char(36) not null
+  country_id char(36) references countries(id)
   );
 grant select on local.locations to public;
